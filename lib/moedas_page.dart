29b774a3951982e8c92ab426f484f33db6e47fcc
Diego França <diego.franca92@gmail.com>
2023-05-02 19:23:52 -0300

    inicialize the app and create a list with coin selection

---
+++ lib/moedas_page.dart
@@ -0,0 +1,55 @@
+import 'package:bitbank_app/repositories/moeda_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'models/moeda.dart';
+
+class MoedasPage extends StatefulWidget {
+  const MoedasPage({super.key});
+
+  @override
+  State<MoedasPage> createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+  final tabela = MoedaRepository.tabela;
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  List<Moeda> selecionadas = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Bit Bank')),
+        body: ListView.separated(
+            itemBuilder: (BuildContext context, int moeda) {
+              return ListTile(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+                leading: selecionadas.contains(tabela[moeda])
+                    ? const CircleAvatar(
+                        child: Icon(Icons.check),
+                      )
+                    : SizedBox(
+                        width: 40, child: Image.asset(tabela[moeda].icone)),
+                title: Text(
+                  tabela[moeda].nome,
+                  style: const TextStyle(
+                      fontSize: 17, fontWeight: FontWeight.w400),
+                ),
+                trailing: Text(real.format(tabela[moeda].preco)),
+                selected: selecionadas.contains(tabela[moeda]),
+                selectedTileColor: Colors.indigo[50],
+                onLongPress: () {
+                  setState(() {
+                    (selecionadas.contains(tabela[moeda]))
+                        ? selecionadas.remove(tabela[moeda])
+                        : selecionadas.add(tabela[moeda]);
+                  });
+                },
+              );
+            },
+            separatorBuilder: (_, __) => const Divider(),
+            padding: const EdgeInsets.all(16),
+            itemCount: tabela.length));
+  }
+}
---
+++ lib/repositories/moeda_repository.dart
@@ -0,0 +1,24 @@
+import 'package:bitbank_app/models/moeda.dart';
+
+class MoedaRepository {
+  static List<Moeda> tabela = [
+    Moeda(
+      icone: 'images/bitcoin.png',
+      nome: 'Bitcoin',
+      sigla: 'BTC',
+      preco: 163483.00,
+    ),
+    Moeda(
+      icone: 'images/ethereum.png',
+      nome: 'Ethereum',
+      sigla: 'ETH',
+      preco: 974.00,
+    ),
+    Moeda(
+      icone: 'images/cardano.png',
+      nome: 'Cardano',
+      sigla: 'ADA',
+      preco: 3456.00,
+    )
+  ];
+}
